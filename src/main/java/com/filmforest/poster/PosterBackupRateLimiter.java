7b65870aed3aa5cea833b106d3e11f072bf555b6
Kkwans <Kkwans@users.noreply.github.com>
2026-09-14 01:38:57 +0800

    fix(poster): 标注限流器配置注入构造器

--- src/main/java/com/filmforest/poster/PosterBackupRateLimiter.java
+++ src/main/java/com/filmforest/poster/PosterBackupRateLimiter.java
@@ -1,5 +1,6 @@
 package com.filmforest.poster;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -15,6 +16,7 @@ public class PosterBackupRateLimiter {
     private final int maxRequests;
     private final Deque<Long> requestTimes = new ArrayDeque<>();
 
+    @Autowired
     public PosterBackupRateLimiter(PosterBackupProperties properties) {
         this(properties.getRateLimitWindow(), properties.getMaxRequestsPerWindow());
     }
